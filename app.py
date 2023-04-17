34eae87aa3642526144aefa66c0a137f859abf57
Condesay <39847380+condesay@users.noreply.github.com>
2023-04-17 10:51:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,161 @@
-import os
-import streamlit as st
-import PyPDF2
+from PyPDF2 import PdfReader
 import pandas as pd
+import os
 import openai
+from io import BytesIO
+from PyPDF2 import PdfReader
+import pandas as pdpd
+from openai.embeddings_utils import get_embedding, cosine_similarity
+import openai
+import os
+import requests
 import redis
+from ast import literal_eval
 import numpy as np
+from typing import List, Iterator
 
-# Définition des constantes
-EMBEDDING_MODEL = "text-embedding-ada-002"
-VECTOR_DIM = 768
-DISTANCE_METRIC = "COSINE"
-INDEX_NAME = "embeddings-index"
-PREFIX = "doc"
-REDIS_HOST = "localhost"
-REDIS_PORT = 6379
 
-# Définition des champs RediSearch
-text = TextField(name="text")
-embedding_field = VectorField(
-    "embedding",
-    "FLAT",
-    {"TYPE": "FLOAT", "SIZE": VECTOR_DIM, "DISTANCE_METRIC": DISTANCE_METRIC},
-)
-fields = [text, embedding_field]
+# Ignore unclosed SSL socket warnings - optional in case you get these errors
+import warnings
 
-# Connexion à Redis
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# Fonction pour extraire le texte d'un fichier PDF et créer un dataframe pandas
-def create_dataframe_from_pdf(pdf_file):
-    with open(pdf_file, "rb") as f:
-        reader = PyPDF2.PdfFileReader(f)
-        pages = [reader.getPage(i) for i in range(reader.getNumPages())]
-        texts = [page.extractText() for page in pages]
-    df = pd.DataFrame({"text": texts})
-    df.drop_duplicates(subset=["text"], keep="first", inplace=True)
-    return df.reset_index(drop=True)
+pdf = PdfReader(open("partp34.pdf", "rb"))
+number_of_pages = len(pdf.pages)
 
-# Fonction pour calculer les embeddings des données textuelles
-def calculate_embeddings(df):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    embeddings = df["text"].apply(
-        lambda x: openai.Embedding.create(input=x, model=EMBEDDING_MODEL)["data"][0]["embedding"]
-    )
-    df["embedding"] = embeddings
+pdf_text = []
+for i in range(number_of_pages):
+    page = pdf.pages[i]
+    text = page.extract_text()
+    if text is not None:
+        pdf_text.append({'text': text})
+        
+def paper_df(pdf):
+    filtered_pdf = []
+    for row in pdf:
+        if len(row['text']) < 30:
+            continue
+        filtered_pdf.append(row)
+    df = pd.DataFrame(filtered_pdf)
     return df
 
-# Fonction pour créer un index RediSearch et indexer les embeddings
-def create_index(df):
-    try:
-        redis_client.ft(INDEX_NAME).info()
-    except redis.exceptions.ResponseError:
-        redis_client.ft_create(
-            INDEX_NAME,
-            *fields,
-            prefix=PREFIX,
-            index_type=IndexType.HASH,
-        )
+df = paper_df(pdf_text)
+print(df)
 
-    for i, row in df.iterrows():
-        redis_client.hset(f"{PREFIX}:{i}", mapping=row.to_dict())
+def calculate_embeddings(df):
+    openai.api_key = os.getenv("OPENAI_API_KEY","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")
+    embedding_model = "text-embedding-ada-002"
+    embeddings = df.text.apply([lambda x: get_embedding(x, engine=embedding_model)])
+    df["embeddings"] = embeddings
+    print('Done calculating embeddings')
+    return df
 
-# Fonction pour effectuer une recherche RediSearch et retourner les résultats
-def search(query, k=10):
-    embedded_query = openai.Embedding.create(input=query, model=EMBEDDING_MODEL)["data"][0]["embedding"]
-    results = redis_client.ft_search(INDEX_NAME, f"@embedding:[{','.join(map(str, embedded_query))}]",
-                                     limit=k, with_scores=True)
-    return results
+df = paper_df(pdf_text)
+df = calculate_embeddings(df)
+print(df)
 
+# Read vectors from strings back into a list
+#df['embeddings']=df.embeddings.apply(literal_eval)
+
+from redis.commands.search.indexDefinition import (
+    IndexDefinition,
+    IndexType,
+)
+from redis.commands.search.query import Query
+from redis.commands.search.field import (
+    TextField,
+    VectorField,
+)
+
+REDIS_HOST ="127.0.0.1"
+REDIS_PORT ="6379"
+REDIS_PASSWORD = "" # default for passwordless Redis
+
+# Connect to Redis
+redis_client = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    password=REDIS_PASSWORD
+)
+
+
+# Constants
+VECTOR_DIM = len(df['embeddings'][0]) # length of the vectors
+VECTOR_NUMBER = len(df)                 # initial number of vectors
+INDEX_NAME = "embeddings-index2"                 # name of the search index
+PREFIX2 = "doc2"                                  # prefix for the document keys
+DISTANCE_METRIC = "COSINE"                      # distance metric for the vectors (ex. COSINE, IP, L2)
+
+# Define RediSearch fields for each of the columns in the dataset
+
+text = TextField(name="text")
+title_embedding = VectorField("embeddings",
+    "FLAT", {
+        "TYPE": "FLOAT32",
+        "DIM": VECTOR_DIM,
+        "DISTANCE_METRIC": DISTANCE_METRIC,
+        "INITIAL_CAP": VECTOR_NUMBER,
+    }
+)
+fields = [text, title_embedding]
+try:
+    redis_client.ft(INDEX_NAME).info()
+    print("Index already exists")
+except:
+    # Create RediSearch Index
+    redis_client.ft(INDEX_NAME).create_index(
+        fields = fields,
+        definition = IndexDefinition(prefix=[PREFIX2], index_type=IndexType.HASH)
+    )
+
+def index_documents(client: redis.Redis, prefix: str, documents: pd.DataFrame):
+    records = documents.to_dict("records")
+    for doc2 in records:
+        key = f"{prefix}:{str(doc2['id'])}"
+
+        # create byte vectors for title and content
+        title_embedding = np.array(doc2["embeddings"], dtype=np.float32).tobytes()
+
+        # replace list of floats with byte vectors
+        doc2["embeddings"] = title_embedding
+
+        client.hset(key, mapping = doc2)
+
+index_documents(redis_client, PREFIX2, df)
+print(f"Loaded {redis_client.info()['db0']['keys']} documents in Redis search index with name: {INDEX_NAME}")
+
+
+def search_redis(
+    redis_client: redis.Redis,
+    user_query: str,
+    index_name: str = "embeddings-index2",
+    vector_field: str = "embeddings",
+    return_fields: list = ["text", "vector_score"],
+    hybrid_fields = "*",
+    k: int = 20,
+) -> List[dict]:
+    EMBEDDING_MODEL = "text-embedding-ada-002"
+
+    # Creates embedding vector from user query
+    embedded_query = openai.Embedding.create(input=user_query,
+                                            model=EMBEDDING_MODEL,
+                                            )["data"][0]['embedding']
+
+    # Prepare the Query
+    base_query = f'{hybrid_fields}=>[KNN {k} @{vector_field} $vector AS vector_score]'
+    query = (
+        Query(base_query)
+         .return_fields(*return_fields)
+         .sort_by("vector_score")
+         .paging(0, k)
+         .dialect(2)
+    )
+    params_dict = {"vector": np.array(embedded_query).astype(dtype=np.float32).tobytes()}
+
+    # perform vector search
+    results = redis_client.ft(index_name).search(query, params_dict)
+    for i, Undoc in enumerate(results.docs):
+        score = 1 - float(Undoc.vector_score)
+        print(f"{i}. {Undoc.title} (Score: {round(score ,3) })")
+    return results.docs 
